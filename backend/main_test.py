1a82d6c78c0c006da1b4a6269fc2faabf2395649
sohan1611 <sohanmandal1611@gmail.com>
2026-06-25 01:30:08 +0530

    Add debug prints

--- backend/main_test.py
+++ backend/main_test.py
@@ -1,10 +1,16 @@
+import sys
+print("STARTING PYTHON SCRIPT...", flush=True)
 import os
+print("OS IMPORTED", flush=True)
 import uvicorn
+print("UVICORN IMPORTED", flush=True)
 from fastapi import FastAPI
+print("FASTAPI IMPORTED", flush=True)
 app = FastAPI()
 @app.get("/")
 def read_root():
     return {"status": "ok", "message": "Dummy test server running"}
 
 if __name__ == "__main__":
+    print("STARTING UVICORN NOW...", flush=True)
     uvicorn.run("backend.main_test:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
